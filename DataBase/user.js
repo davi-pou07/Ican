f5750d7f2209826b8e26e70f607f9aa89de39899
davi-pou07 <davihareis@gmail.com>
2021-02-22 19:59:10 -0300

    heroku

--- DataBase/user.js
+++ DataBase/user.js
@@ -5,18 +5,26 @@ const connection = require("./database")
 const User= connection.define('users',{
     nome:{
         type:Sequelize.STRING,
-        allowNull:true
+        allowNull:false
     },
     email:{
         type:Sequelize.STRING,
-        allowNull:true
+        allowNull:false
     },
     senha:{
 
         type:Sequelize.STRING,
-        allowNull:true
+        allowNull:false
     },
     nivel:{
+        type:Sequelize.INTEGER,
+        allowNull:false
+    },
+    numero:{
+        type:Sequelize.STRING,
+        allowNull:false 
+    },
+    acesso:{
         type:Sequelize.INTEGER,
         allowNull:true
     }
--- admin/userController.js
+++ admin/userController.js
@@ -2,31 +2,52 @@ const express = require('express')
 const User = require("../DataBase/user")
 const Sequelize = require("sequelize")
 const router = express.Router()
-
-router.get("/admin/user",(req,res)=>{
+const bcrypt = require("bcryptjs")
+router.get("/admin/user", (req, res) => {
     res.send("ISAIAS ESTOU EM PAUSA NÃO ME INCOMODE!")
 })
 
-router.get("/admin/new",(req,res)=>{
+
+router.get("/admin/new", (req, res) => {
     res.render("admin/new")
 })
-router.post("/admin/adicionar",(req,res)=>{
+router.post("/admin/adicionar", (req, res) => {
     var nome = req.body.nome
     var email = req.body.email
+    var pass1 = req.body.pass1
     var senha = req.body.senha
     var nivel = req.body.nivel
-    User.create({
-        nome:nome,
-        email:email,
-        senha:senha,
-        nivel:nivel
-    }).then(()=>{
-        res.redirect("/admin/user")
-    })
-router.post("/user/senha",(req,res)=>{
-
-
+    var numero = req.body.telefone
+    if (pass1 == senha && senha != undefined) {
+        User.findOne({ where: { email: email } }).then(emai => {
+            if (emai == undefined) {
+                User.findOne({ where: { numero: numero } }).then(num => {
+                    if (num == undefined) {
+                        var salt = bcrypt.genSaltSync(10)
+                        var hash = bcrypt.hashSync(senha, salt)
+                        User.create({
+                            nome: nome,
+                            email: email,
+                            nivel: nivel,
+                            numero: numero,
+                            senha: hash
+                        }).then(() => {
+                            res.redirect("/admin/user")
+                        }).catch(err => {
+                            res.redirect("/admin/new")
+                        })
+                    } else {
+                        res.redirect("/admin/new")
+                    }
+                })
+            }res.redirect("/admin/new")
+        })
+    }else{
+    }
 })
+
+router.post("/user/senha", (req, res) => {
+
 
 })
 
